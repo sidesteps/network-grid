283f981d63349ec30a8153dc6557489915284382
sidestep <sidestep@gigz.eu>
2021-07-25 16:03:09 +0200

    with java BitSet updated in-place. Returning state-area as sparce matrix with indices of set bits.

--- network-grid.clj
+++ network-grid.clj
@@ -1,60 +1,35 @@
 (ns network-grid
-  (:import java.util.BitSet)
-  (:import java.lang.Integer))
+  (:import java.util.BitSet))
 
-(defn- make-bit-area[nrows ncols] 
-  (-> (vec (repeatedly nrows #(new BitSet ncols)))
-      (with-meta {:rows nrows :cols ncols})))
+(defn- make-state-area[nrows ncols] 
+  (vec (repeatedly nrows #(new BitSet ncols))))
+
+(defn- pr-bit-set [bs]
+  (vec (load-string (str \# bs))))
+
+(defn- pr-state-area[sa]
+  (filter some? (map-indexed #(if (= 0 (.cardinality %2))
+                       nil (vector %1 (pr-bit-set %2))) sa)))
+
+(defn network-grid [nrows ncols commands]
+  (let [sa (make-state-area nrows ncols)] 
+    (doseq [[cmd x1 y1 x2 y2] commands]
+      (doseq [i (range y1 y2)]
+        (cmd (sa i) x1 x2)))
+    (pr-state-area sa)))
 
 (defn- off [bs x1 x2]
-  (.clear bs x1 x2)
-  bs)
+  (.clear bs x1 x2))
 
 (defn- on [bs x1 x2]
-  (.set bs x1 x2)
-  bs)
+  (.set bs x1 x2))
 
 (defn- toggle [bs x1 x2]
-  (.flip bs x1 x2)
-  bs)
+  (.flip bs x1 x2))
 
-(defn network-grid[nrows ncols commands]
-  (let [a (make-bit-area nrows ncols)] 
-    (doseq [[cmd x1 y1 x2 y2] commands]
-      (doseq [row (range y1 y2)]
-        (cmd (a row) x1 x2)))
-    a))
-
-(defn- pr-bit-row [len bit-row]
-  (mapcat #(Integer/toUnsignedString % 2) (.toByteArray bit-row)))
-
-  (comment (def row-0 (vec (repeat len \0)))
-   (def     set-bits (load-string (str "#"(.toString bit-row))))
-     (if (not-empty set-bits)
-       ( (do (println "setyti " set-bits) (persistent! (apply assoc! (transient row-0) (interleave set-bits [\1])))))
-       row-0))
-
-(print set-bits)
-
-(defn pr-bits [bit-area]
-  (let [row-len (:cols (meta bit-area))]
-     (for [bit-row bit-area]
-      (pr-bit-row row-len bit-row))))
-
-(pr-bits grid)
-
-(def commands (load-file "grid.edn"))
-
-(persistent! (assoc! (transient [1 2 3]) 2 10 1 22))
-(def grid (network-grid 10 10 commands))
-(print (map #(.toString %)grid))
-(print set-bits)
-(def bs (-> (new BitSet 100)
-            (toggle 2 90)))
-
-(print (.length bs))
-(doseq [b (.toByteArray bs)]
-       (println b))
-(count (mapcat #(take 8 (reverse (Integer/toUnsignedString % 2))) (.toByteArray bs)))
-(Integer/toString (.toByteArray (new BitSet 10)) 2)
-
+(do
+  (def nrows 20)
+  (def ncols 40)
+  (def commands (load-file "commands.edn"))
+  (def grid (network-grid nrows ncols commands))
+  grid)
